c0fb4b6e7c1196ec9810ebdb7b08ab5fdcb56467
franchesoni <marchesoniacland@gmail.com>
2025-01-15 15:37:10 +0100

    early stopping + validation

--- regression/our_regression.py
+++ regression/our_regression.py
@@ -7,6 +7,7 @@ import logging
 
 from utils import get_dataset, get_optimizer
 from our_methods import get_method
+from model import EarlyStopping
 
 
 class Runner:
@@ -38,6 +39,12 @@ class Runner:
         # obtain training set
         logging.info("Training set info:")
         dataset_object, dataset = get_dataset(args, config, test_set=False)
+        val_set_object, val_set = get_dataset(
+            args, config, test_set=True, validation=True
+        )
+        train_subset_object, train_subset = get_dataset(
+            args, config, test_set=False, validation=True
+        )
         self.dataset_object = dataset_object
         train_loader = data.DataLoader(
             dataset,
@@ -45,21 +52,73 @@ class Runner:
             shuffle=True,
             num_workers=config.data.num_workers,
         )
+        val_loader = data.DataLoader(
+            val_set,
+            batch_size=config.testing.batch_size,
+            num_workers=config.data.num_workers,
+        )
+        train_subset_loader = data.DataLoader(
+            train_subset,
+            batch_size=config.testing.batch_size,
+            num_workers=config.data.num_workers,
+            shuffle=True,
+        )
 
+        from torch.utils.tensorboard import SummaryWriter
+
+        writer = SummaryWriter(log_dir=os.path.join(args.log_path, "mytensorboard"))
+        early_stopper = EarlyStopping(
+            patience=config.diffusion.nonlinear_guidance.patience,
+            delta=config.diffusion.nonlinear_guidance.delta,
+        )
         # load model
         assert dataset_object.train_dim_y == 1
         ys = dataset[:, -1]
-        bounds = (min(ys.min(), -4 * ys.std() + ys.mean()), max(ys.max(), 4 * ys.std() + ys.mean()))
+        bounds = (
+            min(ys.min(), -4 * ys.std() + ys.mean()),
+            max(ys.max(), 4 * ys.std() + ys.mean()),
+        )
         hparams = dict(n_components=32, n_bins=32, n_quantile_levels=32, bounds=bounds)
 
         model = get_method(self.method_name)(
-            [dataset_object.train_dim_x, 100, 50],
-            **hparams
+            [dataset_object.train_dim_x, 100, 50], **hparams
         ).to(config.device)
         optimizer = get_optimizer(self.config.optim, model.parameters())
         train_epochs = config.diffusion.nonlinear_guidance.n_pretrain_epochs
         model.train()
         for epoch in range(train_epochs):
+            step_offset = len(train_subset_loader) * epoch
+            for step, xy_0 in enumerate(train_subset_loader):
+                xy_0 = xy_0.to(config.device)
+                x_batch = xy_0[:, : -self.config.model.y_dim]
+                y_batch = xy_0[:, -self.config.model.y_dim :]
+                optimizer.zero_grad()
+                pred = model(x_batch)
+                loss = model.loss(y_batch, pred)
+                loss.backward()
+                optimizer.step()
+                print(f"epoch {epoch} step {step:03} loss {loss:.3f}", end="\r")
+                writer.add_scalar("Loss/train_subset", loss, step_offset + step)
+            with torch.no_grad():
+                model.eval()
+                val_losses = []
+                for xy_0 in val_loader:
+                    xy_0 = xy_0.to(config.device)
+                    x_batch = xy_0[:, : -self.config.model.y_dim]
+                    y_batch = xy_0[:, -self.config.model.y_dim :]
+                    pred = model(x_batch)
+                    loss = model.loss(y_batch, pred)
+                    val_losses.append(loss)
+                val_loss = torch.stack(val_losses).mean()
+                model.train()
+                writer.add_scalar("Loss/val", val_loss, step_offset)
+                early_stopper(val_cost=val_loss, epoch=epoch)
+            if early_stopper.early_stop:
+                print("Early stopping")
+                best_n_epochs = early_stopper.best_epoch
+                break
+        for epoch in range(best_n_epochs):
+            step_offset = len(train_loader) * epoch
             for step, xy_0 in enumerate(train_loader):
                 xy_0 = xy_0.to(config.device)
                 x_batch = xy_0[:, : -self.config.model.y_dim]
@@ -70,6 +129,8 @@ class Runner:
                 loss.backward()
                 optimizer.step()
                 print(f"epoch {epoch} step {step:03} loss {loss:.3f}", end="\r")
+                writer.add_scalar("Loss/train", loss, step_offset + step)
+
         states = [model.state_dict(), optimizer.state_dict(), hparams]
         torch.save(states, os.path.join(args.log_path, "ckpt.pth"))
 
@@ -111,7 +172,8 @@ class Runner:
         hparams = states[2]
 
         model = get_method(self.method_name)(
-            [dataset_object.train_dim_x, 100, 50], **hparams).to(config.device)
+            [dataset_object.train_dim_x, 100, 50], **hparams
+        ).to(config.device)
         model = model.to(self.device)
         model.load_state_dict(states[0], strict=True)
         model.eval()
