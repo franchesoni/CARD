6e3dc063f3abae132d3e8ff7ad0d13840f34d7ca
franchesoni <marchesoniacland@gmail.com>
2025-01-27 14:57:56 +0100

    hacking run script

--- regression/training_scripts/ours_run.sh
+++ regression/training_scripts/ours_run.sh
@@ -2,23 +2,24 @@ set -e
 export EXP_DIR=./results
 export N_STEPS=1000
 export SERVER_NAME=a4000
-export RUN_NAME=run_ours_ce_dev
-export N_SPLITS=2
+export RUN_NAME=run_ours_21jan
+export N_SPLITS=20
 export N_THREADS=4
 export DEVICE_ID=0
-
 export CAT_F_PHI=_cat_f_phi
 
 # List of datasets
-DATASETS=("uci_boston" "uci_concrete" "uci_energy" "uci_kin8nm" "uci_naval" "uci_power" "uci_wine" "uci_yatch")
+DATASETS=("uci_boston" "uci_concrete" "uci_energy" "uci_kin8nm" "uci_naval" "uci_power" "uci_yacht")
 # "uci_protein"  # needs N_SPLITS=5
 
 # List of loss functions
-LOSSES=("ours.ce")
+LOSSES=("ours.crpshist" "ours.pinball")
+# LOSSES=("ours.laplacecrps" "ours.laplacewb" "ours.gaussian" "ours.crpshist" "ours.iqn")
+# LOSSES=("ours.mdn")
 
 for LOSS in "${LOSSES[@]}"; do
-    export MODEL_VERSION_DIR=our_uci_results/${RUN_NAME}/${LOSS}
     for TASK in "${DATASETS[@]}"; do
+        export MODEL_VERSION_DIR=our_uci_results/${RUN_NAME}/${LOSS}
         echo "Running task: $TASK with loss: $LOSS"
         
         # Run training
